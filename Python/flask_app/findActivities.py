0957bc3ae9e9056b40fda46f8d024a81462e9fd3
amyeasson <s2534866@ed.ac.uk>
2025-11-01 16:30:26 +0000

    added function for translating user categories into API tags

---
+++ Python/flask_app/findActivities.py
@@ -0,0 +1,21 @@
+def translate_tags(categories):
+    # mappings from user selected activities to API tags for 'type'
+    category_mapping = {
+        'Coffee': ['cafe', 'coffee_shop'],
+        'Dinner': ['restaurant'],
+        'Lunch': ['cafe','restaurant'],
+        'Movies': ['movie_theater'],
+        'Museum': ['museum', 'art_gallery'],
+        'Outdoor': ['park','hiking_area','botanical_garden','zoo','picnic_ground'],
+        'Live Events': ['concert_hall','theater','stadium','amphitheater'],
+        'Drinks': ['bar'],
+        'Shopping': ['shopping_mall','department_store','book_store'],
+        # walk should not be included when querying the API
+        'Walk"': ['Walk']
+    }
+
+    translated_types = []
+    for category in categories:
+        if category in category_mapping:
+            translated_types.extend(category_mapping[category])
+        return translated_types
